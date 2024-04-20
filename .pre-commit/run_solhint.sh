3a16b874cbd51de46d3784622e85ab5786a480a8
Your Name <you@example.com>
2024-04-20 23:50:58 +0200

    Updated to SolHint 4.5.4.

--- .pre-commit/run_solhint.sh
+++ .pre-commit/run_solhint.sh
@@ -2,11 +2,11 @@
 
 # Solhint's repo doesn't support pre-commit out-of-the-box, so this script is the workaround.
 
-VERSION="4.0.0"
+VERSION="4.5.4"
 
 if ! which solhint 1>/dev/null || [[ $(solhint --version) != "$VERSION" ]]; then
   echo "Installing solhint@$VERSION"
   npm install -g solhint@$VERSION
 fi
-
 solhint $@
+echo "$(solhint $@)" >
