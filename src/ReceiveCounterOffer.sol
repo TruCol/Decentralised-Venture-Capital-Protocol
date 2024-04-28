2041bb1225c2c28c154957b0682f5116cccb787b
Your Name <you@example.com>
2024-04-28 18:33:03 +0200

    Increased test coverage.

--- src/ReceiveCounterOffer.sol
+++ src/ReceiveCounterOffer.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.8.23; // Specifies the Solidity compiler version.
 import { Tier } from "../src/Tier.sol";
 import { DecentralisedInvestmentManager } from "../../src/DecentralisedInvestmentManager.sol";
+import "forge-std/src/console2.sol"; // Import the console library
 struct Offer {
   address payable _offerInvestor;
   uint256 _investmentAmount;
@@ -9,37 +10,27 @@ struct Offer {
   uint256 _offerDuration; // Time in seconds for project lead to decide
   uint256 _offerStartTime;
   bool _offerIsAccepted;
+  bool _isDecided;
 }
 
 interface Interface {
   function makeOffer(uint16 multiplier, uint256 duration) external payable;
 
-  function getOffer(uint256 offerId) external returns (Offer memory);
-
-  function getOwner() external returns (address payable);
-
   function acceptOrRejectOffer(uint256 offerId, bool accept) external;
+
+  function pullbackOffer(uint256 offerId) external;
 }
 
 contract ReceiveCounterOffer is Interface {
-  uint256 private _offerInvestmentAmount;
   uint16 private _offerMultiplier;
   uint256 private _offerDuration; // Time in seconds for project lead to decide
   uint256 private _offerStartTime;
   bool private _offerIsAccepted;
-  address payable private _owner;
+  bool private _isDecided;
 
-  Offer[] public offers;
+  Offer[] private _offers;
   address private _projectLead;
-
-  /**
-   * Used to ensure only the owner/creator of the constructor of this contract is
-   *   able to call/use functions that use this function (modifier).
-   */
-  modifier onlyOwner() {
-    require(msg.sender == _owner, "The message is sent by someone other than the owner of this contract.");
-    _;
-  }
+  address private _owner;
 
   /**
    * Constructor for creating a Tier instance. The values cannot be changed
@@ -52,33 +43,41 @@ contract ReceiveCounterOffer is Interface {
   }
 
   function makeOffer(uint16 multiplier, uint256 duration) external payable override {
-    offers.push(Offer(payable(msg.sender), msg.value, multiplier, duration, block.timestamp, false));
-  }
-
-  function getOffer(uint256 offerId) public view override returns (Offer memory) {
-    require(offerId < offers.length, "Invalid offer ID");
-    return offers[offerId];
-  }
-
-  function getOwner() public view override returns (address payable) {
-    return _owner;
+    _offers.push(Offer(payable(msg.sender), msg.value, multiplier, duration, block.timestamp, false, false));
   }
 
   function acceptOrRejectOffer(uint256 offerId, bool accept) public override {
     require(msg.sender == _projectLead, "Only project lead can accept offer");
-    // Offer memory offer ==
-    Offer memory offer = getOffer(offerId);
-    require(!offer._offerIsAccepted, "Offer already accepted");
-    require(block.timestamp <= offer._offerStartTime + offer._offerDuration, "Offer expired");
+
+    require(!_offers[offerId]._isDecided, "Offer already rejected or accepted.");
+    require(block.timestamp <= _offers[offerId]._offerStartTime + _offers[offerId]._offerDuration, "Offer expired");
 
     if (accept) {
-      offer._offerIsAccepted = true;
+      // offer._offerIsAccepted = true;
+      _offers[offerId]._offerIsAccepted = true;
+      _offers[offerId]._isDecided = true;
+
       DecentralisedInvestmentManager dim = DecentralisedInvestmentManager(_owner);
-      dim.receiveAcceptedOffer{ value: offer._investmentAmount }(offer._offerInvestor);
+      dim.receiveAcceptedOffer{ value: _offers[offerId]._investmentAmount }(_offers[offerId]._offerInvestor);
 
       // the transaction is rejected e.g. because the investmentCeiling is reached.
     } else {
-      offer._offerIsAccepted = false;
+      _offers[offerId]._offerIsAccepted = false;
+      _offers[offerId]._isDecided = true;
     }
+  }
+
+  function pullbackOffer(uint256 offerId) public override {
+    require(msg.sender == _offers[offerId]._offerInvestor, "Someone other than the investor tried to retrieve offer.");
+    if (_offers[offerId]._isDecided) {
+      require(!_offers[offerId]._offerIsAccepted, "The offer has been accepted, so can't pull back.");
+    } else {
+      require(
+        block.timestamp > _offers[offerId]._offerStartTime + _offers[offerId]._offerDuration,
+        "The offer duration has not yet expired."
+      );
+    }
+
+    payable(_offers[offerId]._offerInvestor).transfer(_offers[offerId]._investmentAmount);
   }
 }
