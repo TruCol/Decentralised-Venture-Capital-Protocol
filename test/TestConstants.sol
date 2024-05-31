5d2a0c7d132eafef700c4ccb1dac29ab9e01a03e
Your Name <you@example.com>
2024-05-31 13:35:21 +0200

    Moved constant into separate TestConstants.sol file.

---
+++ test/TestConstants.sol
@@ -0,0 +1,3 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.25 <0.9.0;
+uint32 constant _MAX_NR_OF_TIERS = 100;
--- test/functional/DecentralisedInvestmentManager/triggerReturnAll/FuzzTriggerReturnAll.t.sol
+++ test/functional/DecentralisedInvestmentManager/triggerReturnAll/FuzzTriggerReturnAll.t.sol
@@ -7,22 +7,16 @@ import { StdCheats } from "forge-std/src/StdCheats.sol";
 
 import { DecentralisedInvestmentManager } from "../../../../src/DecentralisedInvestmentManager.sol";
 import { Helper } from "../../../../src/Helper.sol";
-import { TestMathHelper } from "../../../TestMathHelper.sol";
+
 import { TestInitialisationHelper } from "../../../TestInitialisationHelper.sol";
 import { InitialiseDim } from "test/InitialiseDim.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-uint32 constant _MAX_NR_OF_TIERS = 100;
+import "test/TestConstants.sol";
 
 interface IFuzzTriggerReturnAll {
   function setUp() external;
-
-  function getRandomMultiplesAndCeilings(
-    uint256[_MAX_NR_OF_TIERS] memory randomCeilings,
-    uint8[_MAX_NR_OF_TIERS] memory randomMultiples,
-    uint8 randNrOfInvestmentTiers
-  ) external returns (uint8[] memory multiples, uint256[] memory sameNrOfCeilings);
 
   function testFuzzTriggerReturnAll(
     address projectLead,
@@ -48,55 +42,12 @@ TODO: test whether the investments are:
 */
 contract FuzzTriggerReturnAll is PRBTest, StdCheats, IFuzzTriggerReturnAll {
   address internal _projectLead;
-  TestMathHelper private _testMathHelper;
   TestInitialisationHelper private _testInitialisationHelper;
   Helper private _helper;
 
   function setUp() public virtual override {
-    _testMathHelper = new TestMathHelper();
     _helper = new Helper();
     _testInitialisationHelper = new TestInitialisationHelper();
-  }
-
-  /**
-   * @notice Generates random multiples and corresponding ceiling values for a specified number of investment tiers.
-   * @dev This function selects a random subset of unique investment ceilings and ensures
-   * multiples are greater than 1. It leverages helper functions for sorting, uniquing,
-   * and minimum/maximum calculations.
-   *
-   * @param randomCeilings An array of random ceiling values (unused length, replaced with actual length).
-   * @param randomMultiples An array of random multiples (unused length, replaced with actual length).
-   * @param randNrOfInvestmentTiers The randomly chosen number of investment tiers.
-   *
-   * @return multiples A dynamic array containing the selected random multiples.
-   * @return sameNrOfCeilings A dynamic array containing the corresponding unique ceiling values for each tier.
-   */
-  function getRandomMultiplesAndCeilings(
-    uint256[_MAX_NR_OF_TIERS] memory randomCeilings,
-    uint8[_MAX_NR_OF_TIERS] memory randomMultiples,
-    uint8 randNrOfInvestmentTiers
-  ) public virtual override returns (uint8[] memory multiples, uint256[] memory sameNrOfCeilings) {
-    uint256 nrOfRandomCeilings = randomCeilings.length; // TODO: change this to _MAX_NR_OF_TIERS.
-    // Change the fixed array length from _MAX_NR_OF_TIERS to array of variable length (dynamic array).
-    uint256[] memory duplicateCeilings = new uint256[](nrOfRandomCeilings);
-    for (uint256 i = 0; i < nrOfRandomCeilings; ++i) {
-      // +1 to ensure a ceiling of 0 is shifted to a minimum of 1.
-      duplicateCeilings[i] = _testMathHelper.maximum(1, randomCeilings[i]);
-    }
-    // Removes duplicate values and sorts the ceilings from small to large.
-    uint256[] memory ceilings = _testMathHelper.getSortedUniqueArray(duplicateCeilings);
-
-    // From the selected unique, ascending Tier investment ceilings, select subset of random size.
-    uint256 nrOfInvestmentTiers = (randNrOfInvestmentTiers % ceilings.length) + 1;
-    // Recreate the multiples and ceilings of the selected random size (nr. of Tiers).
-    multiples = new uint8[](nrOfInvestmentTiers);
-    sameNrOfCeilings = new uint256[](nrOfInvestmentTiers);
-    for (uint256 i = 0; i < nrOfInvestmentTiers; ++i) {
-      multiples[i] = uint8(_testMathHelper.maximum(2, randomMultiples[i])); // Filter out the 0 and 1 values.
-      sameNrOfCeilings[i] = ceilings[i];
-    }
-
-    return (multiples, sameNrOfCeilings);
   }
 
   /**
@@ -117,7 +68,7 @@ contract FuzzTriggerReturnAll is PRBTest, StdCheats, IFuzzTriggerReturnAll {
   ) public virtual override {
     uint8[] memory multiples;
     uint256[] memory sameNrOfCeilings;
-    (multiples, sameNrOfCeilings) = getRandomMultiplesAndCeilings({
+    (multiples, sameNrOfCeilings) = _testInitialisationHelper.getRandomMultiplesAndCeilings({
       randomCeilings: randomCeilings,
       randomMultiples: randomMultiples,
       randNrOfInvestmentTiers: randNrOfInvestmentTiers
