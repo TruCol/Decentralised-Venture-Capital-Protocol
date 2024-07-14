662f73b38a4a0cb70fc26b858cdca5c9acbf5ff9
Your Name <you@example.com>
2024-07-14 14:03:26 +0200

    Restored compilability.

--- test/IterableMapping.sol
+++ test/IterableMapping.sol
@@ -50,30 +50,3 @@ library IterableMapping {
     map.keys.pop();
   }
 }
-
-contract TestIterableMap {
-  using IterableMapping for IterableMapping.Map;
-
-  IterableMapping.Map private _map;
-
-  function testIterableMap() public {
-    map.set(address(0), 0);
-    map.set(address(1), 100);
-    map.set(address(2), 200); // insert
-    map.set(address(2), 200); // update
-    map.set(address(3), 300);
-
-    for (uint256 i = 0; i < map.size(); i++) {
-      address key = map.getKeyAtIndex(i);
-      assert(map.get(key) == i * 100);
-    }
-
-    map.remove(address(1));
-
-    // keys = [address(0), address(3), address(2)]
-    assert(map.size() == 3);
-    assert(map.getKeyAtIndex(0) == address(0));
-    assert(map.getKeyAtIndex(1) == address(3));
-    assert(map.getKeyAtIndex(2) == address(2));
-  }
-}
